3ce8621459e3acff53dfd41ae1ee6827e38b97be
rdin21 <umrdin21@gmail.com>
2022-11-23 21:47:00 +0300

    update routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from "react";
 import { Routes, Route } from "react-router-dom";
+import Header from "../components/Header/Header";
+import { useAppSelector } from "../hooks/redux";
+import { taskApi } from "../service/taskService";
+import { TUserFromAccessToken } from "../types/TypeUser";
+import { formatDate } from "../utils/utils";
 import { allRoutes } from "./Routes";
 const AppRoutes: FC = () => {
+  const user = useAppSelector((s) => s.user.data) as TUserFromAccessToken;
+  const { data: tasks, error: getTaskError } = taskApi.useGetTaskByDateQuery(
+    `?date=${formatDate}&userId=${user.id}`
+  );
   return (
-    <Routes>
-      {allRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
-    </Routes>
+    <>
+      <div className="container">
+        <Header userName={user.name} tasks={tasks} getTaskError={getTaskError} />
+      </div>
+      <Routes>
+        {allRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      </Routes>
+    </>
   );
 };
 
